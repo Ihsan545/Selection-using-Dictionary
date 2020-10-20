46df794f0149932ac591f8eb6b02149fbea65095
anwar <Ihsan545>
2020-10-19 19:03:45 -0500

    test passed

--- test_more_fun_with_collections/test_assign_average.py
+++ test_more_fun_with_collections/test_assign_average.py
@@ -6,13 +6,13 @@ from more_fun_with_collections.assign_average import switch_average
 
 class MyTestCase(unittest.TestCase):
     def test_A_switch_average(self):
-
+        grade = "A"
+        score = 100
+        result = 100
         self.assertEqual(result, switch_average(grade), score)
 
     def test_B_switch_average(self):
-        grade1 = "B"
-        score1 = 90
-        result1 = 90
+
         self.assertEqual(result1, switch_average(grade1), score1)
 
     def test_C_switch_average(self):
